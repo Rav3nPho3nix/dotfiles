fc1d5e19bf5b63a29b77860f59aab13284534453
Rav3nPho3nix <128914405+Rav3nPho3nix@users.noreply.github.com>
2024-03-24 16:03:33 +0100

    Delete hypr directory

--- hypr/scripts/maxVolume.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-volume=$(wpctl get-volume 57 | awk '{print $2}')
-if (( $(echo "$volume >= 1.0" | bc) )); then
-	eval "wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 1.0 "
-else
-	eval "wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 2%+ "
-fi
